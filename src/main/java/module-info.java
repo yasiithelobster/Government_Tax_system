bff7f5af77cc477f8f9ee63d969085e85da283eb
yasii <yasirudilmith34@gmail.com>
2025-04-27 18:30:55 +0530

    removed an unwanted class. fixed bugs in test classes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module lk.gov.taxdepartmentapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires javafx.graphics;
 
     opens lk.gov.taxdepartmentapp to javafx.fxml;
     exports lk.gov.taxdepartmentapp;
